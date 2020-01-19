09a36d0220087c5ade39db539da937c8bb8a88e4
Ryandi Pratama Purba <purbaryandi@gmail.com>
2020-01-19 12:47:50 +0700

    belajar operator

---
+++ operator.js
@@ -0,0 +1,11 @@
+function panggilOperator() {
+    var a = 4;
+    var b = 3;
+
+    console.log("4 + 3 = " + (a + b));
+    console.log("4 - 3 = " + (a - b));
+    console.log("4 * 3 = " + (a * b));
+    console.log("4 / 3 = " + (a / b));
+}
+
+panggilOperator();
