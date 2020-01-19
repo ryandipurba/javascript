db4d5d46d8c9506e1d56a4a1e4376869d9e63f44
Ryandi Pratama Purba <purbaryandi@gmail.com>
2020-01-19 13:03:27 +0700

    belajar operator perbandingan

---
+++ op-perbandingan.js
@@ -0,0 +1,11 @@
+var x = 10;
+var z = 11;
+
+console.log(x > z); //lebih besar dari
+console.log(x < z); //lebih kecil dari
+console.log(x >= z); //lebih besar dari dan sama dengan
+console.log(x <= z); //lebih kecil dari dan sama dengan
+console.log(x == z); // sama dengan "tidak peduli type data nya apa"
+console.log(x === z); // sama dengan "type data harus sama"
+console.log(x != z); // tidak sama dengan 
+console.log(x !== z); // tidak sama dengan "type data harus sama"
