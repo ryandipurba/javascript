8aaea0a6be964a16ac8291b54313ae4579fc40dc
Ryandi Pratama Purba <purbaryandi@gmail.com>
2020-01-19 12:57:40 +0700

    belajar increment decrement

---
+++ increment.js
@@ -0,0 +1,23 @@
+// increment (tambah tambah 1)
+
+console.log("contoh increment");
+var x = 0;
+console.log(x);
+x++; //increment
+console.log(x);
+x++; //increment
+console.log(x);
+x++; //increment
+console.log(x);
+
+// decrement (kuran kurang 1)
+
+console.log("contoh decrement");
+var y = 0;
+console.log(y);
+y--; //increment
+console.log(y);
+y--; //increment
+console.log(y);
+y--; //increment
+console.log(y);
